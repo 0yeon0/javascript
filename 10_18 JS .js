bc39c0c3fbef22750917df2edea56ad696d06610
0yeon0 <133744383+0yeon0@users.noreply.github.com>
2023-10-18 16:44:52 +0900

    Add files via upload
    10월 18일 자바스크립트 코드 작성

---
+++ 10_18 JS .js
@@ -0,0 +1,109 @@
+// 1. Function
+function printHello() {
+    console.log('Hello');
+}
+printHello();
+
+function log(message) {
+    console.log(message);
+}
+log('Hello@');
+log(1234);
+
+// TypeScript 배우기 추천
+
+// 2. Parameters
+// premitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+    obj.name = 'coder';
+}
+const ellie = { name: 'ellie' };
+changeName(ellie);
+console.log(ellie);
+
+// 3. Default parameters (added in ES6)
+function showMassage(message, from = 'unknown') {
+    console.log('${massage} by ${from}');
+}
+showMassage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printALL(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+
+    for (const arg of args) {
+        console.log(arg);
+    }
+
+    args.forEach((arg) => console.log(arg));
+}
+printALL('dream', 'coding', 'ellie');
+
+// 5. Local scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+    let message = 'hello';
+    console.log(message); // local variable
+    console.log(globalMessage);
+    function printAnother() {
+        console.log(message);
+        let childMessage = 'hello';
+    }
+    // console.log(childMessage);  // error
+}
+printMessage();
+
+// 6. Return a value
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(1, 2); // 3
+console.log('sum: ${sum(1, 2)}');
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        // long upgrade logic...
+    }
+}
+
+// good
+function upgradeUser(user) {
+    if (user.point <= 10) {
+        return;
+    }
+    // long upgrade logic... 
+}
+
+// 1. Function expression
+const print = function () { // anonymous function
+    console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// 2. Callback function using function expression
+function randomQuiz(answer, printYes, printNo) {
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+const printYes = function () {
+    console.log('yes!');
+};
+
+const printNo = function print() {
+    console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
